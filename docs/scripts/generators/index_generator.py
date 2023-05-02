671e80007f49703d878b5fdc10881529eadc25e6
gwct <greggwct@gmail.com>
2023-05-02 16:11:03 -0400

    first commit

---
+++ docs/scripts/generators/index_generator.py
@@ -0,0 +1,133 @@
+############################################################
+# For personal site, 03.20
+# This generates the file "index.html"
+############################################################
+
+import sys, os
+sys.path.append(os.path.abspath('../lib/'))
+import read_chunks as RC
+
+######################
+# HTML template
+######################
+
+html_template = """
+<!doctype html>
+    {head}
+
+<body>
+	Placeholder
+	<!--
+	<div class="row" id="top_grid">
+		<div class="col-2-24" id="margin"></div>
+		<div class="col-20-24" id="main_header">Turtle genomics</div>
+		<div class="col-2-24" id="margin"></div>
+	</div>
+
+	{nav}
+
+	<div class="row">
+		<div class="col-2-24" id="margin"></div>
+		<div class="col-20-24" id="main_col">
+            <h2>Sample list</h2>
+		</div>
+        <div class="col-2-24" id="margin"></div>
+	</div>
+
+	<center>
+		<div id="table_container">
+			<table id="table_content">
+				{sample_table}
+			</table>
+		</div>
+	</center>
+
+	<div class="row">
+		<div class="col-2-24" id="margin"></div>
+		<div class="col-12-24">
+			<span><em>{total_samples} total samples, minus {excluded_samples} excluded samples (<s>strikethrough</s>) yields a dataset of <b>{final_samples} samples</b>.</em></span>
+		</div>
+		<div class="col-10-24" id="margin"></div>
+	</div>
+
+	<div class="sep_div"></div>
+
+    {footer}
+	-->
+</body>
+"""
+
+######################
+# Main block
+######################
+pagefile = "index.html";
+print("Generating " + pagefile + "...");
+title = "Mus t-haplotype"
+
+head, nav, footer = "", "", "";
+
+# head = RC.readHead(title, pagefile);
+# nav = RC.readNav(pagefile);
+# footer = RC.readFooter();
+
+outfilename = "../../" + pagefile;
+
+samples_file = "../../../data/samples.csv";
+
+sample_table = "";
+first = True;
+num_samples = 0;
+excluded_samples = 0;
+# for line in open(samples_file):
+# 	if line.startswith("#"):
+# 		continue;
+
+# 	line = line.strip().split(",");
+
+# 	if first:
+# 		sample_table += "<thead>";
+# 		sample_table += "<th>Family</th>";
+# 		sample_table += "<th>Species</th>";
+# 		sample_table += "<th>Assembly</th>";
+# 		sample_table += "<th>Coverage</th>";
+# 		sample_table += "<th>Scaffolds</th>";
+# 		sample_table += "<th>Scaffold N50</th>";
+# 		sample_table += "<th>Annotated</th>";
+# 		sample_table += "<th>% Complete BUSCO</th>";
+# 		#sample_table += "<th>Notes</th>";
+# 		sample_table += "</thead>";
+
+# 		first = False;
+# 		continue;
+
+# 	# if line[28] != "tip":
+# 	# 	continue;
+
+# 	if line[24] == "N":
+# 		sample_table += "<tr class='strikeout'>";
+# 		excluded_samples += 1;
+# 	else:
+# 		sample_table += "<tr>";
+
+# 	sample_table += "<td>" + line[0] + "</td>";
+# 	sample_table += "<td><em>" + line[1] + " " + line[2] + "</em></td>";
+# 	sample_table += "<td><a href='" + line[10] + "' target='_blank'>" + line[11] + "</a></td>";
+# 	sample_table += "<td>" + line[12] + "X</td>";
+# 	sample_table += "<td>" + line[19] + "</td>";
+# 	sample_table += "<td>" + line[20] + "</td>";
+# 	sample_table += "<td>" + line[22] + "</td>";
+# 	if line[36] != "NA":
+# 		sample_table += "<td>" + str(round(float(line[37]) / float(line[36]) * 100, 3)) + "</td>";
+# 	else:
+# 		sample_table += "<td>NA</td>";
+# 	#sample_table += "<td>" + line[17] + "</td>";
+# 	if line[24] == "N":
+# 		sample_table += "</s>";
+
+# 	sample_table += "</tr>";
+# 	num_samples += 1;
+
+
+
+with open(outfilename, "w") as outfile:
+    outfile.write(html_template.format(head=head, nav=nav, sample_table=sample_table, total_samples=num_samples, excluded_samples=excluded_samples, final_samples=num_samples-excluded_samples, footer=footer));
---
+++ docs/scripts/make.py
@@ -0,0 +1,74 @@
+import sys, os, argparse
+
+print()
+print("###### Build site pages ######");
+print("PYTHON VERSION: " + ".".join(map(str, sys.version_info[:3])))
+print("# Script call: " + " ".join(sys.argv) + "\n----------");
+
+parser = argparse.ArgumentParser(description="Gets stats from a bunch of abyss assemblies.");
+parser.add_argument("--all", dest="all", help="Build all pages", action="store_true", default=False);
+parser.add_argument("--index", dest="index", help="Without --all: build index.html. With --all: exlude index.html", action="store_true", default=False);
+# parser.add_argument("--assemblies", dest="assemblies", help="Without --all: build assemblies.html. With --all: exlude assemblies.html", action="store_true", default=False);
+# parser.add_argument("--trees", dest="trees", help="Without --all: build trees.html. With --all: exlude trees.html", action="store_true", default=False);
+# parser.add_argument("--annotations", dest="annotations", help="Without --all: build annotations.html. With --all: exlude annotations.html", action="store_true", default=False);
+parser.add_argument("--cactus", dest="cactus", help="Without --all: build cactus.html. With --all: exlude cactus.html", action="store_true", default=False);
+# parser.add_argument("--cactustests", dest="cactustests", help="Without --all: build cactus_tests.html. With --all: exlude cactus_tests.html", action="store_true", default=False);
+# parser.add_argument("--windows", dest="windows", help="Without --all: build windows.html. With --all: exlude windows.html", action="store_true", default=False);
+# parser.add_argument("--analyses", dest="analyses", help="Without --all: build analyses.html. With --all: exlude analyses.html", action="store_true", default=False);
+# parser.add_argument("--people", dest="people", help="Without --all: build people.html. With --all: exlude people.html", action="store_true", default=False);
+# parser.add_argument("--links", dest="links", help="Without --all: build links.html. With --all: exlude links.html", action="store_true", default=False);
+args = parser.parse_args();
+# Input options.
+
+#cwd = os.getcwd();
+os.chdir("generators");
+
+pages = {
+    'index' : args.index,
+    # 'assemblies' : args.assemblies,
+    # 'trees' : args.trees,
+    # 'annotations' : args.annotations,
+    'cactus' : args.cactus,
+    # 'cactustests' : args.cactustests,
+    # 'windows' : args.windows,
+    # 'analyses' : args.analyses,
+    # 'people' : args.people,
+    # 'links' : args.links
+}
+
+if args.all:
+    pages = { page : False if pages[page] == True else True for page in pages };
+
+if pages['index']:
+    os.system("python index_generator.py");
+
+# if pages['annotations']:
+#     os.system("Rscript annotations_generator.r");
+
+# if pages['assemblies']:
+#     os.system("Rscript assemblies_generator.r");
+
+# if pages['trees']:
+#     os.system("Rscript trees_generator.r");
+
+if pages['cactus']:
+    os.system("Rscript cactus_generator.r");
+
+# if pages['cactustests']:
+#     os.system("Rscript cactus_tests_generator.r");
+
+# if pages['windows']:
+#     os.system("Rscript windows_generator.r");
+
+# if pages['analyses']:
+#     os.system("Rscript analyses_generator.r");
+
+# if pages['people']:
+#     os.system("python people_generator.py");
+
+# if pages['links']:
+#     os.system("python links_generator.py");
+    
+print("----------\nDone!");
+
+
